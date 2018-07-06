d192fc4cb79a72e7d05c2d60dd1646bd3ccc6d50
conanzyy <conanzyy@163.com>
2018-07-06 14:43:14 +0800

    1111

---
+++ main.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from show import Ui_Form
+
+if __name__=="__main__":
+    import sys
+    from PyQt5.QtGui import QIcon
+    app=QtWidgets.QApplication(sys.argv)
+    widget=QtWidgets.QWidget()
+    ui=Ui_Form()
+    ui.setupUi(widget)
+    # widget.setWindowIcon(QIcon('web.png'))#增加icon图标，如果没有图片可以没有这句
+    widget.show()
+    sys.exit(app.exec_())
--- show.py
+++ show.py
@@ -27,14 +27,4 @@ class Ui_Form(object):
         Form.setWindowTitle(_translate("Form", "Form"))
         self.pushButton.setText(_translate("Form", "PushButton"))
 
-if __name__=="__main__":
-        import sys
-        from PyQt5.QtGui import QIcon
-        app=QtWidgets.QApplication(sys.argv)
-        widget=QtWidgets.QWidget()
-        ui=Ui_Form()
-        ui.setupUi(widget)
-        # widget.setWindowIcon(QIcon('web.png'))#增加icon图标，如果没有图片可以没有这句
-        widget.show()
-        sys.exit(app.exec_())
 
